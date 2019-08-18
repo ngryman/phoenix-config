b415589d7f4cfa7e69a1af56c3dac37ced177ef8
ngryman <ngryman@gmail.com>
2019-08-18 21:11:06 +0200

    :beetle: fix screens and workspaces

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 import './lib/additions'
 
-import { HYPER_KEY, HYPER_SHIFT_KEY, MBP_SCREEN_UID, BIG_SCREEN_UID, VER_SCREEN_UID } from './lib/constants'
+import { alert } from './lib/utils'
+
+import {
+  HYPER_KEY,
+  HYPER_SHIFT_KEY,
+  MBP_SCREEN_UID,
+  BIG_SCREEN_UID
+} from './lib/constants'
 import * as actions from './actions'
 
 Key.on('g', HYPER_KEY, actions.displayGrid)
@@ -10,10 +17,8 @@ Key.on('space', HYPER_SHIFT_KEY, actions.toggleFullscreen)
 
 Key.on('1', HYPER_KEY, _.partial(actions.moveToScreen, MBP_SCREEN_UID))
 Key.on('2', HYPER_KEY, _.partial(actions.moveToScreen, BIG_SCREEN_UID))
-Key.on('3', HYPER_KEY, _.partial(actions.moveToScreen, VER_SCREEN_UID))
 
-Key.on('1', HYPER_SHIFT_KEY, _.partial(actions.openWorkspace, 'Achieve'))
-Key.on('2', HYPER_SHIFT_KEY, _.partial(actions.openWorkspace, 'Zapier'))
+Key.on('1', HYPER_SHIFT_KEY, _.partial(actions.openWorkspace, 'Zapier'))
 
 Key.on('left', HYPER_KEY, _.partial(actions.moveInGrid, 'left'))
 Key.on('right', HYPER_KEY, _.partial(actions.moveInGrid, 'right'))
